c16c2b043abda9b830f5b98f9da84bbc241f4454
Ed Gorach <gorachedik96@gmail.com>
2016-11-30 08:03:11 +0000

    feature / refractor

--- Factory.rb
+++ Factory.rb
@@ -1,23 +1,21 @@
 class Factory
-    
     def self.new(*attrs, &block)
       Class.new do
         attr_accessor *attrs
       
         define_method :initialize do |*attrs_params|
-          attrs.each_with_index do |attr ,index|
-            self.send("#{attr}=",attrs_params[index])
+          attrs.each_with_index do |attr, index|
+            send("#{attr}=", attrs_params[index])
           end
         end
       
         define_method :[] do |attr|
-          attr.is_a?(Integer)? send("#{attrs[attr]}") : send(attr)
+          attr.is_a?(Numeric) ? send("#{attrs[attr]}") : send(attr)
         end
       
         class_eval(&block) if block_given?
       end
     end  
-    
 end    
 
 Customer = Factory.new(:name, :address) do
